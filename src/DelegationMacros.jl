676629ca290e5396e2efd6c1518ce3128e7129d5
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 09:13:07 -0400

    Update DelegationMacros.jl

--- src/DelegationMacros.jl
+++ src/DelegationMacros.jl
@@ -1,58 +1,5 @@
 module DelegationMacros
 
-"""
-    macros for doing delegation
-    import Base: length, last, (abs), (+)
-    
-    Given these types
-    
-       type MyInt          type MyInts                   type MyNums{T}
-          i::Int              elems::Vector{Int}           elems::T
-       end                 end                           end
-        
-    These macro calls
- 
-       @delegateTyped MyInt.i      [ abs, ]
-       @delegateTyped2 MyInt.i     [ (+), ]
-       
-       @delegate MyInts.elems [ length,  last ]
-       @delegate MyNums.elems [ length,  last ]
-       
-    produces these blocks of expressions
- 
-      abs(a::MyInt)           = MyInt( abs( getfield(a, :i) ) )
-      (+)(a::MyInt, b::MyInt) = MyInt( (+)( getfield(a, :i), getfield(b, :i) ) ) 
- 
-      last( a::MyInts)  = last( getfield(a, :elems) )
-      length(a::MyInts) = length( getfield(a, :elems) )
- 
-      last( a::MyNums)  = last( getfield(a, :elems) )
-      length(a::MyNums) = length( getfield(a, :elems) )
-  
-    and allows
-    
-      myFirstInt = MyInt(-1)
-      mySecondInt = MyInt(2)
-      abs(myFirstInt)          # MyInt(1)
-      myFirstInt + mySecondInt # MyInt(1)
-      
-      myInts = MyInts([5, 4, 3, 2, 1])
-      myNums = MyNums([1.0, 2.0, 3.0])
-      
-      length(myInts) # 5
-      length(myNums) # 3
-      
-      last(myInts)  # 1
-      last(myNums)  # 3.0
-"""->DelegationMacros
-
-#=
-    based on original work by John Myles White and Toivo Henningsson
-    (see end of file for source code refs)
-=#
-
-
-
 export @delegate, @delegate2, @delegateTyped, @delegateTyped2
 
 macro delegate(source, targets)
@@ -125,6 +72,59 @@ macro delegateTyped2(sourceExemplar, targets)
     end
   return Expr(:block, fdefs...)
 end
+
+
+"""
+    macros for doing delegation
+    import Base: length, last, (abs), (+)
+    
+    Given these types
+    
+       type MyInt          type MyInts                   type MyNums{T}
+          i::Int              elems::Vector{Int}           elems::T
+       end                 end                           end
+        
+    These macro calls
+ 
+       @delegateTyped MyInt.i      [ abs, ]
+       @delegateTyped2 MyInt.i     [ (+), ]
+       
+       @delegate MyInts.elems [ length,  last ]
+       @delegate MyNums.elems [ length,  last ]
+       
+    produces these blocks of expressions
+ 
+      abs(a::MyInt)           = MyInt( abs( getfield(a, :i) ) )
+      (+)(a::MyInt, b::MyInt) = MyInt( (+)( getfield(a, :i), getfield(b, :i) ) ) 
+ 
+      last( a::MyInts)  = last( getfield(a, :elems) )
+      length(a::MyInts) = length( getfield(a, :elems) )
+ 
+      last( a::MyNums)  = last( getfield(a, :elems) )
+      length(a::MyNums) = length( getfield(a, :elems) )
+  
+    and allows
+    
+      myFirstInt = MyInt(-1)
+      mySecondInt = MyInt(2)
+      abs(myFirstInt)          # MyInt(1)
+      myFirstInt + mySecondInt # MyInt(1)
+      
+      myInts = MyInts([5, 4, 3, 2, 1])
+      myNums = MyNums([1.0, 2.0, 3.0])
+      
+      length(myInts) # 5
+      length(myNums) # 3
+      
+      last(myInts)  # 1
+      last(myNums)  # 3.0
+"""->DelegationMacros
+
+#=
+    based on original work by John Myles White and Toivo Henningsson
+    (see end of file for source code refs)
+=#
+
 
 end module # DelegationMacros
 
