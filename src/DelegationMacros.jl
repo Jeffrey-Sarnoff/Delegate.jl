8d0a7f646d766c2b4a4fac0d7d0dfe68c40a3b66
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-23 09:42:30 -0400

    Update DelegationMacros.jl

--- src/DelegationMacros.jl
+++ src/DelegationMacros.jl
@@ -2,6 +2,42 @@ module DelegationMacros
 
 export @delegate, @delegate2, @delegateTyped, @delegateTyped2
 
+"""
+
+    macros for doing delegation
+    import Base: length, last
+    
+    Given these types
+    
+       type MyInts                   type MyNums{T}
+           elems::Vector{Int}           elems::T
+       end                           end
+        
+    These macro calls
+ 
+       @delegate MyInts.elems [ length,  last ]
+       @delegate MyNums.elems [ length,  last ]
+       
+    produces these blocks of expressions
+ 
+      last( a::MyInts)  = last( getfield(a, :elems) )
+      length(a::MyInts) = length( getfield(a, :elems) )
+ 
+      last( a::MyNums)  = last( getfield(a, :elems) )
+      length(a::MyNums) = length( getfield(a, :elems) )
+  
+    and allows
+
+      myInts = MyInts([5, 4, 3, 2, 1])
+      myNums = MyNums([1.0, 2.0, 3.0])
+      
+      length(myInts) # 5
+      length(myNums) # 3
+      
+      last(myInts)  # 1
+      last(myNums)  # 3.0
+
+"""     
 macro delegate(source, targets)
   typename = esc(source.args[1])
   fieldname = esc(Expr(:quote, source.args[2].args[1]))
@@ -19,6 +55,42 @@ end
 
 # for methods that take two equi-typed source arguments
 
+"""
+
+    macros for doing delegation
+    import Base: length, last
+    
+    Given these types
+    
+       type MyInts                   type MyNums{T}
+           elems::Vector{Int}           elems::T
+       end                           end
+        
+    These macro calls
+ 
+       @delegate MyInts.elems [ length,  last ]
+       @delegate MyNums.elems [ length,  last ]
+       
+    produces these blocks of expressions
+ 
+      last( a::MyInts)  = last( getfield(a, :elems) )
+      length(a::MyInts) = length( getfield(a, :elems) )
+ 
+      last( a::MyNums)  = last( getfield(a, :elems) )
+      length(a::MyNums) = length( getfield(a, :elems) )
+  
+    and allows
+
+      myInts = MyInts([5, 4, 3, 2, 1])
+      myNums = MyNums([1.0, 2.0, 3.0])
+      
+      length(myInts) # 5
+      length(myNums) # 3
+      
+      last(myInts)  # 1
+      last(myNums)  # 3.0
+
+"""     
 macro delegate2(sourceExemplar, targets)
   typesname = esc(sourceExemplar.args[1])
   fieldname = esc(Expr(:quote, sourceExemplar.args[2].args[1]))
@@ -38,6 +110,34 @@ end
 
 # for methods that take one typed argument and return an iso-typed result
 
+"""
+
+    macros for doing type re-wrapped delegation
+    import Base: (abs), (+)
+    
+    Given the type
+    
+       type MyInt
+          i::Int
+       end
+        
+    These macro calls
+ 
+       @delegateTyped MyInt.i      [ abs, ]
+       @delegateTyped2 MyInt.i     [ (+), ]
+       
+    produces these blocks of expressions
+ 
+      abs(a::MyInt)           = MyInt( abs( getfield(a, :i) ) )
+      (+)(a::MyInt, b::MyInt) = MyInt( (+)( getfield(a, :i), getfield(b, :i) ) ) 
+ 
+      myFirstInt = MyInt(-1)
+      mySecondInt = MyInt(2)
+
+      abs(myFirstInt)          # MyInt(1)
+      myFirstInt + mySecondInt # MyInt(1)
+
+"""
 macro delegateTyped(source, targets)
   typename = esc(source.args[1])
   fieldname = esc(Expr(:quote, source.args[2].args[1]))
@@ -57,6 +157,34 @@ end
 
 # for methods that take two equi-typed source arguments) and return an iso-typed result
 
+"""
+
+    macros for doing type re-wrapped delegation
+    import Base: (abs), (+)
+    
+    Given the type
+    
+       type MyInt
+          i::Int
+       end
+        
+    These macro calls
+ 
+       @delegateTyped MyInt.i      [ abs, ]
+       @delegateTyped2 MyInt.i     [ (+), ]
+       
+    produces these blocks of expressions
+ 
+      abs(a::MyInt)           = MyInt( abs( getfield(a, :i) ) )
+      (+)(a::MyInt, b::MyInt) = MyInt( (+)( getfield(a, :i), getfield(b, :i) ) ) 
+ 
+      myFirstInt = MyInt(-1)
+      mySecondInt = MyInt(2)
+
+      abs(myFirstInt)          # MyInt(1)
+      myFirstInt + mySecondInt # MyInt(1)
+
+"""
 macro delegateTyped2(sourceExemplar, targets)
   typesname = esc(sourceExemplar.args[1])
   fieldname = esc(Expr(:quote, sourceExemplar.args[2].args[1]))
@@ -73,53 +201,6 @@ macro delegateTyped2(sourceExemplar, targets)
   return Expr(:block, fdefs...)
 end
 
-"""
-
-    macros for doing delegation
-    import Base: length, last, (abs), (+)
-    
-    Given these types
-    
-       type MyInt          type MyInts                   type MyNums{T}
-          i::Int              elems::Vector{Int}           elems::T
-       end                 end                           end
-        
-    These macro calls
- 
-       @delegateTyped MyInt.i      [ abs, ]
-       @delegateTyped2 MyInt.i     [ (+), ]
-       
-       @delegate MyInts.elems [ length,  last ]
-       @delegate MyNums.elems [ length,  last ]
-       
-    produces these blocks of expressions
- 
-      abs(a::MyInt)           = MyInt( abs( getfield(a, :i) ) )
-      (+)(a::MyInt, b::MyInt) = MyInt( (+)( getfield(a, :i), getfield(b, :i) ) ) 
- 
-      last( a::MyInts)  = last( getfield(a, :elems) )
-      length(a::MyInts) = length( getfield(a, :elems) )
- 
-      last( a::MyNums)  = last( getfield(a, :elems) )
-      length(a::MyNums) = length( getfield(a, :elems) )
-  
-    and allows
-    
-      myFirstInt = MyInt(-1)
-      mySecondInt = MyInt(2)
-      abs(myFirstInt)          # MyInt(1)
-      myFirstInt + mySecondInt # MyInt(1)
-      
-      myInts = MyInts([5, 4, 3, 2, 1])
-      myNums = MyNums([1.0, 2.0, 3.0])
-      
-      length(myInts) # 5
-      length(myNums) # 3
-      
-      last(myInts)  # 1
-      last(myNums)  # 3.0
-      
-"""->@delegate(source, targets)
 
 
 #=
